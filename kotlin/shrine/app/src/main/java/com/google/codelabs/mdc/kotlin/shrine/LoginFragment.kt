9afc7531130f8135f3d4ce8c41eb02db67917947
Wajahat Karim <wajahatkarim3@gmail.com>
2018-07-17 19:04:20 +0500

    completed first codelab

--- kotlin/shrine/app/src/main/java/com/google/codelabs/mdc/kotlin/shrine/LoginFragment.kt
+++ kotlin/shrine/app/src/main/java/com/google/codelabs/mdc/kotlin/shrine/LoginFragment.kt
@@ -2,9 +2,12 @@ package com.google.codelabs.mdc.kotlin.shrine
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.text.Editable
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import kotlinx.android.synthetic.main.shr_login_fragment.*
+import kotlinx.android.synthetic.main.shr_login_fragment.view.*
 
 /**
  * Fragment representing the login screen for Shrine.
@@ -17,8 +20,35 @@ class LoginFragment : Fragment() {
 
         // Snippet from "Navigate to the next Fragment" section goes here.
 
-        return inflater.inflate(R.layout.shr_login_fragment, container, false)
+        val view = inflater.inflate(R.layout.shr_login_fragment, container, false)
+
+        /// Set an error is password is less than 8 chars
+        view.next_button.setOnClickListener {
+            if (!isPasswordValid(password_edit_text.text!!))
+                password_text_input.error = getString(R.string.shr_error_password)
+            else
+            {
+                password_text_input.error = null
+
+                // Navigate to next frag
+                (activity as NavigationHost).navigateTo(ProductGridFragment(), false)
+            }
+        }
+
+        // Clear error once more than 8 chars are typed
+        view.password_edit_text.setOnKeyListener { view, i, keyEvent ->
+            if (isPasswordValid(password_edit_text.text))
+            {
+                password_text_input.error = null
+            }
+            false
+        }
+
+        return view
     }
 
     // "isPasswordValid" from "Navigate to the next Fragment" section method goes here
+    private fun isPasswordValid(text: Editable?) : Boolean = text != null && text.length >= 8
+
+
 }
